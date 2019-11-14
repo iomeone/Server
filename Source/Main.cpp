4644db0fda750f40b808dd65dcce931f9075e4ea
iomeone <iomeonee@gmail.com>
2019-11-14 13:12:37 +0800

    Update Main.cpp

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -191,6 +191,10 @@ public:
 			for (auto i = 0; i < _actree.getNumChildren(); i++)
 			{
 				Identifier curNameIdenty = _actree.getChild(i).getType();
+				log("curNameIdenty");
+				log(curNameIdenty.toString().getCharPointer());
+				log("_actree.getChild(i)");
+				log(_actree.getChild(i).getType().toString().getCharPointer());
 				if (curNameIdenty == nameId)
 				{
 					pwd = _actree.getChild(i).getProperty("pwd").toString();
